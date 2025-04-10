2e13c256cb73e701d15c808816d7460471af50b9
Vaishnavi Sisodiya <vaishvi.sisodiya28@gmail.com>
2025-04-11 02:27:05 +0530

    frontend

---
+++ frontend/ecommerce_website/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './DescriptionBox.css'
+ 
+const DescriptionBox = () => {
+  return (
+    <div className='description'>
+       <div className='descriptionbox-navigator'>
+         <div className='descriptionbox-nav-box'>
+             Description
+         </div>
+         <div className='descriptionbox-nav-box fade'>
+             Reviews (122)
+         </div>
+       </div>
+       <div className='descriptionbox-description'>
+           <p>An ecommerce website is an online store where customers can find products, browse offerings, and place purchases online. It facilitates the transaction between a buyer and seller. A digital storefront can serve as the virtual equivalent of the product shelves, sales staff, and cash register of a physical shop.</p>
+           <p>E-commerce (electronic commerce) is the buying and selling of goods and services, or the transmitting of funds or data, over an electronic network, primarily the internet.</p>
+        </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
